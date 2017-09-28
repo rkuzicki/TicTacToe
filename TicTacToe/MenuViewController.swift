50d756b8ca9282a08ccd2aedadf937c13c87187f
Radek <rkuzicki@gmail.com>
2017-09-28 20:44:20 +0200

    add board, images and settings for display

---
+++ TicTacToe/MenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Radoslaw Kuzicki on 26/09/2017.
+//  Copyright © 2017 radek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Menu loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- TicTacToe/TwoPlayersViewController.swift
+++ TicTacToe/TwoPlayersViewController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  TwoPlayersViewController.swift
 //  TicTacToe
 //
-//  Created by Radoslaw Kuzicki on 26/09/2017.
+//  Created by Radoslaw Kuzicki on 28/09/2017.
 //  Copyright © 2017 radek. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class TwoPlayersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print("Game loaded")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
