902a89f81f6d34b99de9e83cda2bac6fd1f7141b
Radek <rkuzicki@gmail.com>
2017-09-29 19:58:16 +0200

    add win-lose-draw algorithm for two players

--- TicTacToe/TwoPlayersViewController.swift
+++ TicTacToe/TwoPlayersViewController.swift
@@ -10,19 +10,56 @@ import UIKit
 
 class TwoPlayersViewController: UIViewController {
     
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    var boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    //cross in 1, circle is 2
+    let winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ]
     var crossTurn = true
+    var moves = 0
+    var activeGame = true
     @IBAction func buttonPressed(_ sender: AnyObject) {
-        if (crossTurn) {
-            sender.setImage(UIImage(named: "cross.png")?.withRenderingMode(.alwaysTemplate), for: [])
-            (sender as? UIButton)?.tintColor = UIColor.red
-            crossTurn = false
+        if (activeGame) {
+            var won = false
+            if (crossTurn) {
+                sender.setImage(UIImage(named: "cross.png")?.withRenderingMode(.alwaysTemplate), for: [])
+                (sender as? UIButton)?.tintColor = UIColor.red
+                crossTurn = false
+                boardArray[sender.tag] = 1
+            }
+            else {
+                sender.setImage(UIImage(named: "circle.png")?.withRenderingMode(.alwaysTemplate), for: [])
+                (sender as? UIButton)?.tintColor = UIColor.blue
+                crossTurn = true
+                boardArray[sender.tag] = 2
+            }
+            (sender as? UIButton)?.isUserInteractionEnabled = false
+            moves += 1
+            if (moves > 4) {
+                for com in winningCombinations {
+                    if boardArray[com[0]] > 0 && boardArray[com[0]] == boardArray[com[1]] && boardArray[com[0]] == boardArray[com[2]] {
+                        if (crossTurn == false) {
+                            resultLabel.text = "Crosses win"
+                            won = true
+                            activeGame = false
+                        
+                        }
+                        else {
+                            resultLabel.text = "Circles win"
+                            won = true
+                            activeGame = false
+                        }
+                    }
+                }
+            }
+            if !won && moves == 9 {
+                resultLabel.text = "DRAW"
+                activeGame = false
+            }
+            
         }
-        else {
-            sender.setImage(UIImage(named: "circle.png")?.withRenderingMode(.alwaysTemplate), for: [])
-            (sender as? UIButton)?.tintColor = UIColor.blue
-            crossTurn = true
-        }
-        (sender as? UIButton)?.isUserInteractionEnabled = false
     }
     
     override func viewDidLoad() {
@@ -33,5 +70,4 @@ class TwoPlayersViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
